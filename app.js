285b359717b8950a09a956cc81823dabb3501b2d
Manlangit <manlangitv@gmail.com>
2022-03-06 21:29:29 -0500

    slider page

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Hello there');
+
+const wrapper = document.querySelector('.sliderWrapper');
+const menuItems = document.querySelectorAll('.menuItem');
+
+menuItems.forEach((item, index) => {
+   item.addEventListener('click', () => {
+      wrapper.style.transform = `translateX(${-100 * index}vw)`;
+   });
+});
