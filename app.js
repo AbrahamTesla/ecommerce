074a863b78a7e1b2c7471e81e39dbd6ccc4997ba
Manlangit <manlangitv@gmail.com>
2022-03-10 08:09:21 -0500

    products array, colors and picture

--- app.js
+++ app.js
@@ -1,4 +1,80 @@
-console.log('Hello there');
+const products = [
+   {
+      id: 1,
+      title: 'Hoodie Yellow',
+      price: 50,
+      colors: [
+         {
+            code: 'yellow',
+            img: './img/hoodieYellow.png',
+         },
+         {
+            code: 'blue',
+            img: './img/hoodieBlue.png',
+         },
+      ],
+   },
+   {
+      id: 2,
+      title: 'Hoodie Red',
+      price: 70,
+      colors: [
+         {
+            code: 'green',
+            img: './img/hoodieGreen.png',
+         },
+         {
+            code: 'black',
+            img: './img/hoodieOrange.png',
+         },
+      ],
+   },
+   {
+      id: 3,
+      title: 'Hoodie Blue',
+      price: 80,
+      colors: [
+         {
+            code: 'blue',
+            img: './img/hoodieBlue.png',
+         },
+         {
+            code: 'blue',
+            img: './img/hoodieRed.png',
+         },
+      ],
+   },
+   {
+      id: 4,
+      title: 'Hoodie Orange',
+      price: 90,
+      colors: [
+         {
+            code: 'orange',
+            img: './img/hoodieOrange.png',
+         },
+         {
+            code: 'blue',
+            img: './img/hoodieBlue.png',
+         },
+      ],
+   },
+   {
+      id: 5,
+      title: 'Hoodie Red',
+      price: 100,
+      colors: [
+         {
+            code: 'red',
+            img: './img/hoodieRed.png',
+         },
+         {
+            code: 'blue',
+            img: './img/hoodieYellow.png',
+         },
+      ],
+   },
+];
 
 const wrapper = document.querySelector('.sliderWrapper');
 const menuItems = document.querySelectorAll('.menuItem');
